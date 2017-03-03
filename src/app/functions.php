bec3a88ca9a5971fd34580850f423a946c209ddb
Muah <muah003@gmail.com>
2017-03-03 08:03:00 +0200

    add functions

---
+++ src/app/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * trim phrases in english or arabic
+ */
+if (!function_exists('trimfy')) {
+    function trimfy($value, $limit = 100, $end = ' ...')
+    {
+        $value = strip_tags($value);
+
+        if (crntLang() != 'en') {
+            return strlen($value) <= $limit
+                ? $value
+                : mb_substr($value, 0, $limit, 'UTF-8').$end;
+        }
+
+        return mb_strwidth($value, 'UTF-8') <= $limit
+            ? $value
+            : rtrim(mb_strimwidth($value, 0, $limit, '', 'UTF-8')).$end;
+    }
+}
+
+/*
+ * slug titles for english or arabic
+ */
+if (!function_exists('slugfy')) {
+    function slugfy($string, $separator = '-')
+    {
+        $url = trim($string);
+        $url = strtolower($url);
+        $url = str_replace(' ', $separator, $url);
+        $url = preg_replace('|[^a-z-A-Z\p{Arabic}0-9 _]|iu', '', $url);
+
+        return $url;
+    }
+}
